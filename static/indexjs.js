e3ec99b822f9dc7e45bfdeb0f068ce3302e48a77
zeroIQ2018 <76695802+zeroIQ2018@users.noreply.github.com>
2022-10-25 14:07:17 +0200

    added CSS and made it impossible to  spam it with spaces

---
+++ static/indexjs.js
@@ -0,0 +1,23 @@
+function checkEmpty(field){
+	if (field == "" ||
+		field == null ||
+		field == "undefinied"){
+
+		return false;
+	}
+	else if(/^\s*$/.test(field)){
+		return false;
+	}
+	else{
+		return true;
+	}
+}
+
+document.getElementById("form").onsubmit = function(){
+
+	if(!(checkEmpty(document.getElementById("usernameinput").value || document.getElementById("messageinput")))){
+		alert("Fill the field!");
+		return false; //stop the submit
+	}
+
+};
