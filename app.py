9190513d712141da7444708d150cad2b010a9b82
zero2118 <schneidermax2118@gmail.com>
2022-10-30 19:17:17 +0100

    added date submitted

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-
+import datetime
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
-app.debug = True
+app.debug = False
 db = SQLAlchemy(app)
 
 
@@ -14,16 +14,19 @@ class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(50))
     username = db.Column(db.String(50))
+    date_posted = db.Column(db.String(50))
 
-    def __init__(self, username, message):
+
+    def __init__(self, username, message, date_posted):
         self.message = message
         self.username = username
+        self.date_posted = date_posted
 
 
 
 @app.route("/add_message", methods=["POST"])
 def add_message():
-    message = Message(request.form["Content"], request.form["Username"])
+    message = Message(request.form["Content"], request.form["Username"], datetime.datetime.now())
     db.session.add(message)
     db.session.commit()
     return redirect(url_for("index"))
