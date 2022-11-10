411b627586da88885ff1bc357ec36ebc656506f2
zero2018 <76695802+zeroIQ2018@users.noreply.github.com>
2022-11-10 12:51:22 +0100

    why

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+app.config["SQLALCHEMY_DATABASE_URI"] = "SQLALCHEMY_DATABASE_URI"
 app.debug = False
 db = SQLAlchemy(app)
 
