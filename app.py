11a8a8bbc0a5cef4e4aa2e3db7d23e9ada8b5594
zero2018 <76695802+zeroIQ2018@users.noreply.github.com>
2022-11-11 13:21:31 +0100

    added heroku postgresql config

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import datetime
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+app.config["SQLALCHEMY_DATABASE_URI"] = "DATABASE_URL"
 app.debug = False
 db = SQLAlchemy(app)
 
