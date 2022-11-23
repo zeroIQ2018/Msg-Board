a3e2a378307afb6d468d191e3528c8d80be7363d
zero2118 <76695802+zeroIQ2018@users.noreply.github.com>
2022-11-23 15:42:37 +0100

    repair

--- static/options.js
+++ static/options.js
@@ -1,12 +1,10 @@
 function darkmode(){
     localStorage.setItem('mode', "1");
     a = localStorage.getItem("mode");
-    console.log(a);
 }
 
 
 function lightmode(){
     localStorage.setItem('mode', null);
     a = localStorage.getItem("mode");
-    console.log(a);
 }
