2fdb610196e7719c0a90353b62d6163f139295c9
zero2018 <76695802+zeroIQ2018@users.noreply.github.com>
2022-11-26 10:36:33 +0100

    added signup and login

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import login_required, login_manager, UserMixin, login_user
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 import datetime
-import urllib.request 
 import os
-
+import urllib.request 
+import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 
 def checkifinternet():
     try:
@@ -13,11 +17,18 @@ def checkifinternet():
     except:
         return False
 
+
+
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.debug = True
 app.config["DEBUG_MODE"] = True
+app.config["SECRET_KEY"] = "Pringelsandcoke"
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 if checkifinternet() == True:
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://vppilujfsfwfbe:82c4177c3e7c07709fa25801f62506bf1bfc8d42a585d48260c57ef4be64c2f8@ec2-52-211-232-23.eu-west-1.compute.amazonaws.com:5432/d9n2b1qv12ouoo"
 elif checkifinternet() == False:
@@ -25,7 +36,9 @@ elif checkifinternet() == False:
 db = SQLAlchemy(app)
 
 
-
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -42,38 +55,107 @@ class Message(db.Model):
         self.imageurl = imageurl
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(1000), unique = True)
-    password = db.Column(db.String(1000))
+    username = db.Column(db.String(20), nullable=False, unique = True)
+    password = db.Column(db.String(80), nullable=False)
 
 
-    def __init__(self, username, password ):
-        self.username = username
-        self.password = password
+class Signinform(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=40)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=40)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Register')
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError('That username already exists. Please choose a different one.')
+
+
+class Loginform(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=40)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=40)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login')
 
 
 
+
+
+#MAIN ADDING MESSAGES AND OTHER STUFF
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    user = User.query.filter_by(username="").first()
+    return render_template("home.html")
+
+@app.route("/board", methods=["POST", "GET"])
+def board():
     posts = Message.query.all()
-    return render_template("index.html", posts=posts)
+    return render_template("board.html", posts=posts)
+
+@app.route("/add_message", methods=["GET", "POST"])
+@login_required
+def add_message():
+    message = Message(current_user.username , request.form["Username"], datetime.datetime.now(), request.form["imgurl"])
+    db.session.add(message)
+    db.session.commit()
+    return redirect(url_for("board"))
+
+@app.route("/options" ,methods=["GET", "POST"])
+@login_required
+def options():
+    return render_template("options.html")
 
 
-@app.route("/admin2")
-def fakeadmin():
-    return render_template("fakeadmin.html")
 
 
+
+#LOGIN AND SIGNUP STUFF
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = Signinform()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+
+    return render_template('signup.html', form=form)
+
+@app.route("/login" , methods=["GET", "POST"])
+def login():
+    form = Loginform()
+
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('board'))
+
+
+    return render_template("login.html", form=form)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+
+
+
+#ADMIN STUFF
 @app.route("/admin", methods=["GET", "POST"])
+@login_required
 def admin():
     return render_template("admin.html")
 
-
-
-
 @app.route("/delete", methods=["GET", "POST"])
+@login_required
 def delete():
     db.session.query(Message).delete()
     db.session.commit()
@@ -81,54 +163,13 @@ def delete():
 
 
 @app.route("/deleteid", methods=["POST","GET"])
+@login_required
 def deleteid():
     data = request.form.get('idform', 0)
     int(data)
     Message.query.filter(Message.id == data).delete()
     db.session.commit()
     return redirect(url_for("admin"))
-
-
-@app.route("/add_message", methods=["GET", "POST"])
-def add_message():
-    message = Message(request.form["Content"], request.form["Username"], datetime.datetime.now(), request.form["imgurl"])
-    db.session.add(message)
-    db.session.commit()
-    return redirect(url_for("index"))
-
-
-@app.route("/options" ,methods=["GET", "POST"])
-def options():
-    return render_template("options.html")
-
-
-@app.route("/signup", methods=["GET", "POST"])
-def signup():
-    return render_template("signup.html")
-
-@app.route("/login" , methods=["GET", "POST"])
-def login():
-    return render_template("login.html")
-
-
-@app.route("/doesitexist")
-def doesitexist():
-    givenname = request.form.postf("username")
-    givenpassword = request.form.get("password")
-
-
-@app.route("/addaccount", methods=["GET", "POST"])
-def addaccount():
-    account = User(request.form["username"], request.form["password"])
-    db.session.add(account)
-    db.session.commit()
-    return redirect(url_for("index"))
-
-
-#@app.route("/logout", methods=["GET", "POST"])
-#@login_required
-#def logout():
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/indexjs.js
+++ static/indexjs.js
@@ -15,30 +15,12 @@ function checkEmpty(field){
 
 document.getElementById("form").onsubmit = function(){
 
-	if(!(checkEmpty(document.getElementById("usernameinput").value || document.getElementById("messageinput")))){
+	if((checkEmpty(document.getElementById("messageinput")))){
 		alert("Fill the field!");
 		return false; 
 	}
 
-};
-
-
-(function() // to make the thing load on reload
-{
-  if( window.localStorage )
-  {
-    if( !localStorage.getItem('firstLoad') )
-    {
-      localStorage['firstLoad'] = true;
-      window.location.reload();
-    }  
-    else
-      localStorage.removeItem('firstLoad');
-  }
-})();
-
-
-
+}
 
 
 var str = document.getElementById("youtube").src;
