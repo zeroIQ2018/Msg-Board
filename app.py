c830f09a91f31e362cae9d85f69c4bde2200b365
zero2018 <76695802+zeroIQ2018@users.noreply.github.com>
2022-11-26 12:02:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -78,8 +78,8 @@ class Signinform(FlaskForm):
 
 
 class Loginform(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=40)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=8, max=40)], render_kw={"placeholder": "Password"})
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField('Login')
 
 
